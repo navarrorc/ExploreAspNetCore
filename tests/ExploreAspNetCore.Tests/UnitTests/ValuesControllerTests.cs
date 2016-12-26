ce967801ea0097465353f26b48ac0c0ed44d8604
Roberto C Navarro <rnavarro70@gmail.com>
2016-12-25 23:00:26 -0800

    Initial commit

---
+++ tests/ExploreAspNetCore.Tests/UnitTests/ValuesControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ExploreAspNetCore.api;
+using Xunit;
+
+namespace ExploreAspNetCore.Tests.UnitTests
+{
+    [Trait("Sample", "tests")]
+    public class ValuesControllerTests
+    {
+        [Fact(DisplayName = "Passing Unit Test")]
+        public void Passing_Unit_Test()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.Equal(2, result.Count());
+        }
+
+    }
+}
